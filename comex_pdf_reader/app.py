3f47753b30b31e36984c6ed03047598097e5db18
ThiagoElux01 <thiago.farias@electrolux.com>
2026-02-09 10:05:08 -0300

    Update app.py

--- comex_pdf_reader/app.py
+++ comex_pdf_reader/app.py
@@ -27,8 +27,6 @@ def main():
         home.render()
     elif page == "Processar PDFs":
         process_pdfs.render()
-    elif page == "Arquivos modelo":
-        downloads_page.render()
     elif page == "Configurações":
         settings_page.render()
     else:
