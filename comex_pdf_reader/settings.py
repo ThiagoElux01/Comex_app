50f08891f5c5ad6f278827ee1acb9efcb2cd0aa2
ThiagoElux01 <thiago.farias@electrolux.com>
2026-02-09 10:03:57 -0300

    Update settings.py

--- comex_pdf_reader/settings.py
+++ comex_pdf_reader/settings.py
@@ -1,7 +1,3 @@
-
 import streamlit as st
-
 APP_NAME = st.secrets.get("app", {}).get("name", "COMEX PDF READER")
-
-# Páginas do app
-PAGES = ["Home", "Processar PDFs", "Arquivos modelo", "Configurações"]
+PAGES = ["Home", "Processar PDFs", "Configurações"]
