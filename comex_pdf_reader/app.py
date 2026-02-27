b2ef5c7c9ba1585f05f611d13bb3f1e6074ce003
ThiagoElux01 <thiago.farias@electrolux.com>
2026-02-27 15:51:15 -0300

    Update app.py

--- comex_pdf_reader/app.py
+++ comex_pdf_reader/app.py
@@ -1,8 +1,9 @@
+# app.py
 import streamlit as st
 from auth import is_authenticated
 from ui.login import render_login
 from ui.layout import app_header, sidebar_navigation
-from settings import PAGES
+from settings import PAGES, APP_NAME
 from ui.pages import home, process_pdfs, settings_page
 from ui.pages import downloads_page
 from ui.pages import app_archivo_gastos
@@ -14,22 +15,28 @@ def main():
         render_login()
         return
 
-    app_header()
-
-    # 1) lê a página escolhida
+    # 1) Ler a página escolhida (ANTES do header)
     page = sidebar_navigation(PAGES)
 
-    # 3) se foi disparada navegação por botão, priorize esse destino
+    # 2) Se houve navegação por botão, priorizar esse destino
     if st.session_state.get("_goto_page"):
         page = st.session_state.pop("_goto_page")
 
-    # 4) roteamento
+    # 3) Definir o título dinâmico do cabeçalho
+    header_title = APP_NAME  # padrão
+    if page == "Aplicación Archivo Gastos":
+        header_title = "Limpieza Plantilla Gastos"  # título em espanhol, como você pediu
+
+    # 4) Renderizar o cabeçalho com o título escolhido
+    app_header(title=header_title)
+
+    # 5) Roteamento
     if page == "Home":
         home.render()
     elif page == "Aplicación Comex":
         process_pdfs.render()
-    elif page == "Aplicación Archivo Gastos":   # <--- NOVO
-        app_archivo_gastos.render()             # <--- NOVO
+    elif page == "Aplicación Archivo Gastos":
+        app_archivo_gastos.render()
     elif page == "Configurações":
         settings_page.render()
     else:
